46c3eae2d9651628d3ad5894afdce6ce70b840b5
VanujaDesilva <137886786+VanujaDesilva@users.noreply.github.com>
2023-11-19 18:56:35 +0530

    Update Student.java

--- src/main/java/com/example/ood_cw/Student.java
+++ src/main/java/com/example/ood_cw/Student.java
@@ -3,5 +3,55 @@ public class Student {
     private int stdId;
     private String firstName;
     private String lastName;
-    private
+    private int telNo;
+    private String dob;
+
+    public Student(int stdId, String firstName, String lastName, int telNo, String dob) {
+        this.stdId = stdId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.telNo = telNo;
+        this.dob = dob;
+    }
+
+    public int getStdId() {
+        return stdId;
+    }
+
+    public void setStdId(int stdId) {
+        this.stdId = stdId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getTelNo() {
+        return telNo;
+    }
+
+    public void setTelNo(int telNo) {
+        this.telNo = telNo;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
 }
+
