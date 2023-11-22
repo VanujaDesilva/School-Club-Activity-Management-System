6fb97ad6ae5bd593255acfebf5665671caa6b747
Vanuja De Silva <vanuja.kithpura@gmail.com>
2023-11-22 19:34:09 +0530

    Create attendance class

---
+++ src/main/java/com/example/ood_cw/Attendance.java
@@ -0,0 +1,45 @@
+package com.example.ood_cw;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.stage.Stage;
+
+public class Attendance extends Application {
+
+    @FXML
+    private ChoiceBox<Club> clubSelector;
+
+    @FXML
+    private ChoiceBox<Event> eventSelector;
+
+    @FXML
+    private TableColumn<Student,String> name;
+
+    @FXML
+    private TableColumn<?, ?> status;
+
+    @FXML
+    private TableColumn<?, ?> stdId;
+
+    @FXML
+    private TableColumn<?, ?> telNo;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("attendanceTracking.fxml")));
+        Scene scene = new Scene(fxmlLoader.load(), 900,600 );
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+}
