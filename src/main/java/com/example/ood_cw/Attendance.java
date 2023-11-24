db60c913822a2fee7fac732d609dfc025e161898
Vanuja De Silva <vanuja.kithpura@gmail.com>
2023-11-25 01:01:20 +0530

    update the show button in Attendance class

--- src/main/java/com/example/ood_cw/Attendance.java
+++ src/main/java/com/example/ood_cw/Attendance.java
@@ -2,6 +2,7 @@ package com.example.ood_cw;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -16,14 +17,14 @@ import javafx.stage.Stage;
 import static com.example.ood_cw.HelloController.events;
 import java.net.URL;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Attendance implements Initializable {
-
     public ChoiceBox eventSelector;
     public ChoiceBox clubSelector;
+    public Button showButton;
     @FXML
     private TableView<Student> attendanceTable;
-    private ObservableList<String> eNameList;
     @FXML
     private Button saveButton;
 
@@ -31,6 +32,7 @@ public class Attendance implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
         TableColumn<Student, String> stdIdCol = new TableColumn<>("Student ID");
         TableColumn<Student, String> firstNameCol = new TableColumn<>("First Name");
         TableColumn<Student, String> lastNameCol = new TableColumn<>("Last Name");
@@ -41,20 +43,17 @@ public class Attendance implements Initializable {
         this.data = FXCollections.observableArrayList(
                 new Student("S001", "Jacob", "Smith", "0704594151", "2003-01-23"),
                 new Student("S002", "Emma", "Johnson", "0712345678", "2002-05-15"));
-
+        List<String> events = Arrays.asList("Event1", "Event2", "Event3");
+        eventSelector.getItems().addAll(events);
+        List<String> clubs = Arrays.asList("Club 1", "Club 2", "Club 3");
+        clubSelector.getItems().addAll(clubs);
         stdIdCol.setCellValueFactory(new PropertyValueFactory<>("stdId"));
         firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
         lastNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
         telNoCol.setCellValueFactory(new PropertyValueFactory<>("telNo"));
         dobCol.setCellValueFactory(new PropertyValueFactory<>("dob"));
         attendanceStatusCol.setCellValueFactory(new PropertyValueFactory<Student,String>("status"));
-        this.attendanceTable.setItems(this.data);
 
-        for (int i = 0; i < events.size() ; i++) {
-            Event event = new Event();
-            eNameList.add(event.getEventName());
-        }
-        this.eventSelector.setItems(eNameList);
     }
 
 
@@ -77,5 +76,17 @@ public class Attendance implements Initializable {
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void onShowButtonClick() {
+        String selectedClub = (String) clubSelector.getValue();
+        String selectedEvent = (String) eventSelector.getValue();
+
+        // Check if both club and event are selected
+        if (selectedClub != null && selectedEvent != null) {
+            this.attendanceTable.setItems(this.data);
+        } else {
+            System.out.println("Please select both club and event");
+        }
     }
 }
