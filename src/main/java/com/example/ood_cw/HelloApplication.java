ebaae9633ec979e4a20166b89a45f38f14993de7
YERAN0105 <150114281+YERAN0105@users.noreply.github.com>
2023-11-25 16:25:48 +0530

    updated hello application

--- src/main/java/com/example/ood_cw/HelloApplication.java
+++ src/main/java/com/example/ood_cw/HelloApplication.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        DatabaseConnect.getSchedule();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("Hello!");
