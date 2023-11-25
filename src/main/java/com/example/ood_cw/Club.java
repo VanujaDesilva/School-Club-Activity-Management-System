63eadcd58fa8370919289124fb4c01a015a5706b
Tharusha02 <tjpf17@gmail.com>
2023-11-25 16:40:49 +0530

    Adding the validation to the inputs - Create Club
    Added most of the validations for the user inputs when creating a club

--- src/main/java/com/example/ood_cw/Club.java
+++ src/main/java/com/example/ood_cw/Club.java
@@ -1,16 +1,16 @@
 package com.example.ood_cw;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Club {
+    public static List<List<Object>> mainList = new ArrayList<>();
     public TextField clubName;
     public DatePicker clubDate;
     public TextField clubMission;
@@ -23,10 +23,16 @@ public class Club {
     public Button clubCreate;
     public Button clubDelete;
     public Button imageInsert;
+    public Label nameTick;
+    public Label dateTick;
+    public Label emailTick;
+    public Label contactTick;
+    public Label presidentTick;
+    public Label advisorTick;
 
 
     private String name;
-    private LocalDate foundingDate;
+    private DatePicker foundingDate;
     private String mission;
     private String description;
     private String email;
@@ -35,7 +41,7 @@ public class Club {
     private String presidentName;
     private String clubAdvisorName;
 
-    Club clubInstance = new Club();
+
 
 
     public String getName() {
@@ -47,10 +53,10 @@ public class Club {
     }
 
     public LocalDate getFoundingDate() {
-        return foundingDate;
+        return foundingDate.getValue();
     }
 
-    public void setFoundingDate(LocalDate foundingDate) {
+    public void setFoundingDate(DatePicker foundingDate) {
         this.foundingDate = foundingDate;
     }
 
@@ -111,6 +117,161 @@ public class Club {
     }
 
     public void onCreateButtonClick() throws IOException {
+        Club clubInstance = new Club();
+
+        outer:
+        while (true){
+            int errorCheck = 0;
+            int emptyCheck = 0;
+
+            //setting the club name
+            //checking if the club name is empty
+            if(!clubName.getText().trim().isEmpty()) {
+                //checking if the same club name exists
+                for (List<Object> a : mainList) {
+                    if (a.get(0).equals(clubInstance.getName())) {
+                        clubName.setStyle("-fx-text-fill: red;");
+                        clubInstance.nameTick.setText("\u2717");
+                    } else {
+                        clubInstance.setName(clubName.getText());
+                        clubName.setStyle("-fx-text-fill: green;");
+                        clubInstance.nameTick.setText("\u2713");
+                    }
+                }
+            }
+            else {
+                clubName.setStyle("-fx-text-fill: red;");
+                clubInstance.nameTick.setText("\u2605");
+            }
+
+            //setting the club date
+            clubDate.valueProperty().addListener((observable, oldValue, newValue) -> {
+                //checking if the founding date is empty
+                if (newValue != null) {
+                    //checking if the entered date is valid
+                    if (isValidDate(newValue)){
+                        clubInstance.setFoundingDate(clubDate);
+                        clubDate.setStyle("-fx-text-fill: green;");
+                        clubInstance.dateTick.setText("\u2713"); //tick
+                    }
+                    else {
+                        clubDate.setStyle("-fx-text-fill: red;");
+                        clubInstance.dateTick.setText("\u2717"); //cross
+                    }
+                }
+                else {
+                    clubDate.setStyle("-fx-text-fill: red;");
+                    clubInstance.dateTick.setText("\u2605");  //star
+                }
+            });
+
+
+            //setting the club mission
+            clubInstance.setMission(clubMission.getText());
+
+            //setting the club description
+            clubInstance.setDescription(clubDescription.getText());
+
+            //setting the club president
+            //checking if the club president name is empty
+            if(!clubPresident.getText().trim().isEmpty()) {
+                //checking if the same club president exists
+                for (List<Object> b : mainList) {
+                    if (b.get(4).equals(clubInstance.getPresidentName())) {
+                        clubPresident.setStyle("-fx-text-fill: red;");
+                        clubInstance.presidentTick.setText("\u2717");
+                    } else {
+                        clubInstance.setPresidentName(clubPresident.getText());
+                        clubPresident.setStyle("-fx-text-fill: green;");
+                        clubInstance.nameTick.setText("\u2713");
+                    }
+                }
+            }
+            else {
+                clubPresident.setStyle("-fx-text-fill: red;");
+                clubInstance.presidentTick.setText("\u2605");
+            }
+
+            //setting the club advisor
+            //checking if the club advisor name is empty
+            if(!clubAdvisor.getText().trim().isEmpty()) {
+                //checking if the same club advisor exists
+                for (List<Object> c: mainList){
+                    if (c.get(5).equals(clubInstance.getClubAdvisorName())){
+                        clubAdvisor.setStyle("-fx-text-fill: red;");
+                        clubInstance.advisorTick.setText("\u2717");
+                    }
+                    else {
+                        clubInstance.setClubAdvisorName(clubAdvisor.getText());
+                        clubAdvisor.setStyle("-fx-text-fill: green;");
+                        clubInstance.advisorTick.setText("\u2713");
+                    }
+                }
+            }
+            else {
+                clubAdvisor.setStyle("-fx-text-fill: red;");
+                clubInstance.advisorTick.setText("\u2605");
+            }
+
+
+            //setting the club email
+            //checking if the club email is empty
+            if (!clubEmail.getText().trim().isEmpty()) {
+                //checking if the entered email is valid
+                if (isValidEmail(clubEmail.getText())){
+                    clubInstance.setEmail(clubEmail.getText());
+                    clubEmail.setStyle("-fx-text-fill: green;");
+                    clubInstance.emailTick.setText("\u2713");
+                }
+                else {
+                    clubEmail.setStyle("-fx-text-fill: red;");
+                    clubInstance.emailTick.setText("\u2717");
+                }
+            }
+            else {
+                clubEmail.setStyle("-fx-text-fill: red;");
+                clubInstance.emailTick.setText("\u2605");
+            }
+
+
+            clubContactNo.textProperty().addListener((observable, oldValue, newValue) -> {
+                //checking if the founding date is empty
+                if (newValue != null) {
+                    if (isValidContactNo(newValue)) {
+                        String contactNumFull = "+94" + newValue;
+                        clubInstance.setContactNo(contactNumFull);
+                        clubContactNo.setStyle("-fx-text-fill: green;");
+                        clubInstance.contactTick.setText("\u2713");
+                    } else {
+                        clubContactNo.setStyle("-fx-text-fill: red;");
+                        clubInstance.contactTick.setText("\u2717");
+                    }
+                }
+                else {
+                    clubContactNo.setStyle("-fx-text-fill: red;");
+                    clubInstance.contactTick.setText("\u2605");
+                }
+            });
+
+
+            //clubInstance.setIcon();
+
+
+
+        }
+    }
+
+    private boolean isValidContactNo(String newValue) {
+    }
+
+    private boolean isValidDate(LocalDate date) {
+        return !date.isAfter(LocalDate.now());
+    }
+
+    private boolean isValidEmail(String emailEntered) {
+        //using a regular expression for the validation
+        String emailRegExpress = "^[A-Za-z0-9+_.-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}$";
+        return email.matches(emailRegExpress);
     }
 
     public void onDeleteButtonClick() throws IOException{
