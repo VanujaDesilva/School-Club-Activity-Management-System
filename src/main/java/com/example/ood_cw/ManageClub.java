c78d297f0339374f097b9ea12643eaef8652921e
Tharusha02 <tjpf17@gmail.com>
2023-11-26 06:54:11 +0530

    Added content to 'ManageClub.fxml'
    Added the necessary functionalities to 'ManageClub.fxml'

--- src/main/java/com/example/ood_cw/ManageClub.java
+++ src/main/java/com/example/ood_cw/ManageClub.java
@@ -1,4 +1,41 @@
 package com.example.ood_cw;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
 public class ManageClub {
+    public TextField updateClubName;
+    public DatePicker updateClubDate;
+    public TextField updateClubMission;
+    public TextArea updateClubDescription;
+    public TextField updateClubPresident;
+    public TextField updateClubAdvisor;
+    public TextField updateClubEmail;
+    public TextField updateClubContactNo;
+    public ImageView updateClubIcon;
+    public Button showClub;
+
+    public TextField showName;
+    public Button updateClub;
+    public Label uNameTick;
+    public Label uPresidentTick;
+    public Label uAdvisorTick;
+    public Label uEmailTick;
+    public Label uContactTick;
+    public Label uDateTick;
+    public Label showTick;
+    public Button uInsertIcon;
+
+    public void onShowClubButtonClick(ActionEvent actionEvent) {
+    }
+
+
+
+    public void onUpdateButtonClick(ActionEvent actionEvent) {
+    }
+
+
+    public void onUpdateIconClick(ActionEvent actionEvent) {
+    }
 }
