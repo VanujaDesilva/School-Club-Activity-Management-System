f086da94c1a87212a4c8d52c0c93e0daa34d6fb4
Tharusha02 <tjpf17@gmail.com>
2023-11-28 20:30:30 +0530

    Update club icon part

--- src/main/java/com/example/ood_cw/HelloController.java
+++ src/main/java/com/example/ood_cw/HelloController.java
@@ -1331,7 +1331,7 @@ public class HelloController{
         clubMenuPane.getChildren().setAll(pane);
     }
 
-    
+
     public void onShowClubClick() throws IOException{
         AnchorPane pane = FXMLLoader.load(getClass().getResource("showClubs.fxml"));
         clubMenuPane.getChildren().setAll(pane);
